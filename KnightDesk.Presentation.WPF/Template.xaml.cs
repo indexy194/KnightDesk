299cb3d920263706c8fb5e25ef66623df80c5633
Đào Duy <duydao0221@gmail.com>
2025-08-26 04:43:11 +0700

    Add project files.

---
+++ KnightDesk.Presentation.WPF/Template.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace KnightDesk.Presentation.WPF
+{
+    /// <summary>
+    /// Interaction logic for Template.xaml
+    /// </summary>
+    public partial class Template : Window
+    {
+        public Template()
+        {
+            InitializeComponent();
+        }
+        private void Border_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+                this.DragMove();
+        }
+    }
+}
